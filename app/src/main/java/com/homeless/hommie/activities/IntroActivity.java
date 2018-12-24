3b1e860c21dd394ca1f2d1c9b6cacf27a5f5ae41
Hakan <hknyln37@gmail.com>
2018-12-24 22:39:38 +0300

    IntroActivityde düzenlemeler yapıldı.Dil değiştirildi.

--- app/src/main/java/com/homeless/hommie/activities/IntroActivity.java
+++ app/src/main/java/com/homeless/hommie/activities/IntroActivity.java
@@ -19,10 +19,10 @@ public class IntroActivity extends AppIntro {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addSlide(AppIntroFragment.newInstance("Welcome to Movie Guide", "Find and Discover your favourite Movies, TV Shows, Actors and more.", R.drawable.ic_launcher_large, Color.DKGRAY));
-        addSlide(AppIntroFragment.newInstance("Favorites", "Mark your Movies and TV Shows favourite.\nSo you never miss them again.", R.drawable.heart256, Color.DKGRAY));
-        addSlide(AppIntroFragment.newInstance("Explore", "Search your loved Movies and TV Shows from the movie database.", R.drawable.search256, Color.DKGRAY));
-        addSlide(AppIntroFragment.newInstance("Share", "Share your Movies and TV Shows with friends.", R.drawable.share256, Color.DKGRAY));
+        addSlide(AppIntroFragment.newInstance("Watchlist'e Hoşgeldin", "Find and Discover your favourite Movies, TV Shows, Actors and more.", R.drawable.ic_launcher_large, Color.DKGRAY));
+        addSlide(AppIntroFragment.newInstance("Favoriler", "Mark your Movies and TV Shows favourite.\nSo you never miss them again.", R.drawable.heart256, Color.DKGRAY));
+        addSlide(AppIntroFragment.newInstance("Keşfet", "Search your loved Movies and TV Shows from the movie database.", R.drawable.search256, Color.DKGRAY));
+        addSlide(AppIntroFragment.newInstance("Paylaş", "Share your Movies and TV Shows with friends.", R.drawable.share256, Color.DKGRAY));
 
         showStatusBar(false);
         showSkipButton(true);
